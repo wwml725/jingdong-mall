866d1940afdc8e15cf327c3f2816580bb9c92999
wwml725 <wwml725@163.com>
2019-10-28 07:45:56 +0800

    0002.从网上

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="tab">
     <router-link tag="div" class="tab-item" to="/home">
+      <i class="iconfont icon-shouye"></i>
       <span class="tab-link">首页</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/classify">
+      <i class="iconfont icon-fenlei"></i>
       <span class="tab-link">分类</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/vip">
+      <i class="iconfont icon-VIP"></i>
       <span class="tab-link">Vip</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/cart">
+      <i class="iconfont icon-cart2"></i>
       <span class="tab-link">购物车</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/profile">
+      <i class="iconfont icon-profile1
+"></i>
       <span class="tab-link">个人中心</span>
     </router-link>
   </div>
@@ -42,19 +48,30 @@
   @import "~common/stylus/variable"
   .tab
     display: flex
-    height: 44px
-    line-height 44px
+    height: 50px
+    line-height 22px
     font-size: $font-size-medium
     background-color: #ffffff
     .tab-item
       flex: 1
       text-align center
+      display flex;
+      flex-direction column;
+      .iconfont
+        flex:2
+        line-height:30px
+        font-size:20px
       .tab-link
+        flex:1 ;
+        line-height:20px
+        font-size:6px
+
         /*padding-bottom:5px*/
-        color: #60666b
+        /*color: #60666b*/
       &.router-link-active
-        .tab-link
-          color: $color-theme
-          border-bottom: 2px solid $color-theme
+          color: red
+          font-weight:bold
+          background:#cccccc
+        /*font-size :20px*/
 
 </style>
